20159c5f3b28dd790d3d1896f15cb47c95d69fef
Egor-Vasilevskiy <70340503+Smailent@users.noreply.github.com>
2022-05-04 14:51:20 +0300

    Add calc

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,20 @@
-"use strict"
-// // появление меню для устройст пк
-// const header = document.querySelector('header');
-// const prevScrollPos = window.pageYOffset;
 
-// if(window.matchMedia("only screen and (min-width: 760px)").matches) {
-//   window.onscroll = function() {
-//     let currentScrollPos = window.pageYOffset;
-//     if (window.pageYOffset > 100) {
-//           header.style.top = '0'
-//       } else {
-//           header.style.top = '-100%';
-//       }
-//     prevScrollPos = currentScrollPos;
-//   };
-//   } else {
-//   //mobile device
-//   }
+const header = document.querySelector('header');
+const prevScrollPos = window.pageYOffset;
+
+if(window.matchMedia("only screen and (max-width: 760px)").matches) {
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (window.pageYOffset > 100) {
+          header.style.top = '0px'
+      } else {
+          header.style.top = '-100px';
+      }
+    prevScrollPos = currentScrollPos;
+  };
+  } else {
+  //mobile device
+  }
 
 // Открытие бургер меню
 const iconMenu = document.querySelector('.menu__icon');
