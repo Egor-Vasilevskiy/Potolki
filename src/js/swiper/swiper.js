9c5622d12449f4f3d79e9c8a80d440872175b4da
Egor-Vasilevskiy <70340503+Smailent@users.noreply.github.com>
2022-05-30 22:25:27 +0300

    fix

--- src/js/swiper/swiper.js
+++ src/js/swiper/swiper.js
@@ -1,22 +1,22 @@
 //Swiper Sale
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
 });
 
 var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
 });
 
 
@@ -26,6 +26,7 @@ var swiper = new Swiper(".mySwiper3", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
+  roundLengths : true,
   slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
@@ -35,16 +36,14 @@ var swiper = new Swiper(".mySwiper3", {
     slideShadows: true,
   },
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
 });
 
 
 //Swiper swiperChandelier
 var swiper = new Swiper('.tabs__item', {
   slidesPerView: 1,
-  spaceBetween: 20,
+  roundLengths : true,
+  grabCursor: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
